f1e5ae9f43d8e03adef443c0c56bbce3fba26565
Tommy Ho <aomenlao@gmail.com>
2015-05-03 16:58:01 +0800

    Initial Commit

---
+++ PetDairy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PetDairy
+//
+//  Created by Tommy on 2015-05-03.
+//  Copyright (c) 2015 H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PetDairy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PetDairy
+//
+//  Created by Tommy on 2015-05-03.
+//  Copyright (c) 2015 H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
